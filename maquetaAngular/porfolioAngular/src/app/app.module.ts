c5815d1262230180f323cfb2711f3e180da0ba28
kairayary <kaira.abreu@gmail.com>
2022-09-08 20:29:57 -0300

    creación de componentes

--- maquetaAngular/porfolioAngular/src/app/app.module.ts
+++ maquetaAngular/porfolioAngular/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SoftSkillsComponent } from './componentes/soft-skills/soft-skills.component';
+import { HardSkillsComponent } from './componentes/hard-skills/hard-skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SoftSkillsComponent,
+    HardSkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
